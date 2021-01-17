171eb206e7f984d1b36bad43825468707b8509d6
Enrique(mac) <ejschwarzkopf@gmail.com>
2021-01-16 17:54:49 -0800

    LDhat output analysis scripts

---
+++ 1.scripts/LDhatOutputAnalysis.R
@@ -0,0 +1,85 @@
+##############################################
+##### R script that obtains all relevant #####
+##### information from LDhot output and  #####
+##### and places it in a new table. This #####
+##### script runs over the 500 itera-    #####
+##### tions of a particular set of para- #####
+##### meters.                            #####
+##############################################
+
+##############################################
+##### Load required packages.            #####
+##############################################
+
+
+##############################################
+##### Recieve command line name for in-  #####
+##### put files. Input template should   #####
+##### be the filename up until the ite-  #####
+##### ration number. The script will     #####
+##### fill in the iteration number and   #####
+##### the file extensions (ldhot stan-   #####
+##### dard extensions).                  #####
+##############################################
+
+args = commandArgs(trailingOnly=TRUE)
+
+if(length(args)<1){
+	stop("Missing argument")
+}
+
+filetemplate<-args[1]
+
+itercount<-args[2]
+
+
+##############################################
+##### For loop that goes over all file   #####
+##### names for a set of parameters.     #####
+##############################################
+
+# Standard deviation of all mean rates (per locus)
+Rates_mean_SD_all<-c()
+# Coefficient of variation for all mean rates
+Rates_mean_CV_all<-c()
+# Standard deviation of mean rates between the two loci
+Rates_mean_SD_loci<-c()
+# Coefficient of variation of mean rates between the two loci
+Rates_mean_CV_loci<-c()
+# Same as above, but the median
+Rates_median_SD_all<-c()
+Rates_median_CV_all<-c()
+Rates_median_SD_loci<-c()
+Rates_median_CV_loci<-c()
+# Same as above, but U95-L95
+Rates_95range_SD_all<-c()
+Rates_95range_CV_all<-c()
+Rates_95range_SD_loci<-c()
+Rates_95range_CV_loci<-c()
+
+for(i in 1:itercount){
+	# generate the filename from the template provided as input
+	filename<-paste(filetemplate, i, ".res.txt", sep='')
+	ogtable<-read.table(filename, header=TRUE)
+	# Subset by positions between the two loci
+	table<-ogtable[which(ogtable[,1]>=10 & ogtable[,1]<=15),]
+
+	Rates_mean_SD_all<-c(Rates_mean_SD_all, sd(ogtable[,2]))
+	Rates_mean_CV_all<-c(Rates_mean_CV_all, sd(ogtable[,2])/mean(ogtable[,2]))
+	Rates_mean_SD_loci<-c(Rates_mean_SD_loci, sd(table[,2]))
+	Rates_mean_CV_loci<-c(Rates_mean_CV_loci, sd(table[,2])/mean(table[,2]))
+	Rates_median_SD_all<-c(Rates_median_SD_all, sd(ogtable[,3]))
+	Rates_median_CV_all<-c(Rates_median_CV_all, sd(ogtable[,3])/mean(ogtable[,2]))
+	Rates_median_SD_loci<-c(Rates_median_SD_loci, sd(table[,3]))
+	Rates_median_CV_loci<-c(Rates_median_CV_loci, sd(table[,3])/mean(table[,3]))
+	Rates_95range_SD_all<-c(Rates_95range_SD_all, sd(ogtable[,5]-ogtable[,4]))
+	Rates_95range_CV_all<-c(Rates_95range_CV_all, sd(ogtable[,5]-ogtable[,4])/mean(ogtable[,5]-ogtable[,4]))
+	Rates_95range_SD_loci<-c(Rates_95range_SD_loci, sd(table[,5]-table[,4]))
+	Rates_95range_CV_loci<-c(Rates_95range_CV_loci, sd(table[,5]-table[,4])/mean(table[,5]-table[,4]))
+}
+
+OutputTable<-data.frame(Rates_mean_SD_all=Rates_mean_SD_all, Rates_mean_CV_all=Rates_mean_CV_all, Rates_mean_SD_loci=Rates_mean_SD_loci, Rates_mean_CV_loci=Rates_mean_CV_loci, Rates_median_SD_all=Rates_median_SD_all, Rates_median_CV_all=Rates_median_CV_all, Rates_median_SD_loci=Rates_median_SD_loci, Rates_median_CV_loci=Rates_median_CV_loci, Rates_95range_SD_all=Rates_95range_SD_all, Rates_95range_CV_all=Rates_95range_CV_all, Rates_95range_SD_loci=Rates_95range_SD_loci, Rates_95range_CV_loci=Rates_95range_CV_loci)
+
+output_filename<-paste(filetemplate, ".res.summary.txt", sep='')
+
+write.table(OutputTable, filetemplate, quote=FALSE)
---
+++ 1.scripts/LDhatOutputAnalysisJob.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=LDhatSummary
+#SBATCH --partition=popgenom,kamiak,cas,cahnrs
+#SBATCH --output=/data/cornejo/projects/e.jimenezschwarzkop/SLiMRecombinationModel/1.scripts/1.log/LDhatSummary.out
+#SBATCH --error=/data/cornejo/projects/e.jimenezschwarzkop/SLiMRecombinationModel/1.scripts/1.log/LDhatSummary.err
+#SBATCH --time=7-00:00:00
+#SBATCH --nodes=1
+#SBATCH --workdir="/data/cornejo/projects/e.jimenezschwarzkop/SLiMRecombinationModel"
+#SBATCH --array=1-900
+
+n=$SLURM_ARRAY_TASK_ID
+
+module load r/4.0.2
+
+filepath=$(lsworkspace -v | tail -n 4 | head -n 1)
+
+filetemplate=$(echo ${filepath}/4.LDhat_output/case_${n}/SLiM_case_${n}_)
+
+Rscript --vanilla 1.scripts/LDhatOutputAnalysis.R ${filetemplate} 500
