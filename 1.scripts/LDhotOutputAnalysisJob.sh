eb0979b48b14528e2321a2ecfaa3015e1bb02350
Schwarzkopf Kratzer_ Enrique <e.jimenezschwarzkop@login-p1n01.kamiak.wsu.edu>
2021-01-21 10:27:20 -0800

    Dealing with some cases that didn't run

--- 1.scripts/LDhotOutputAnalysisJob.sh
+++ 1.scripts/LDhotOutputAnalysisJob.sh
@@ -6,9 +6,11 @@
 #SBATCH --time=7-00:00:00
 #SBATCH --nodes=1
 #SBATCH --workdir="/data/cornejo/projects/e.jimenezschwarzkop/SLiMRecombinationModel"
-#SBATCH --array=1-900
+#SBATCH --array=1-36
 
-n=$SLURM_ARRAY_TASK_ID
+i=$SLURM_ARRAY_TASK_ID
+
+n=$(awk 'FNR == '$i' {print}' tmp2.txt)
 
 module load r/4.0.2
 
