8829f672f64b269e55a55eef3e9ac0c37efd9f02
Enrique(mac) <ejschwarzkopf@gmail.com>
2021-01-29 11:37:36 -0800

    Removed array lines

--- 1.scripts/LDhatParameterReductionJob.sh
+++ 1.scripts/LDhatParameterReductionJob.sh
@@ -6,14 +6,8 @@
 #SBATCH --time=7-00:00:00
 #SBATCH --nodes=1
 #SBATCH --workdir="/data/cornejo/projects/e.jimenezschwarzkop/SLiMRecombinationModel"
-#SBATCH --array=1-900
 
-n=$SLURM_ARRAY_TASK_ID
 
 module load r/4.0.2
-
-#filepath=$(lsworkspace -v | tail -n 4 | head -n 1)
-
-#filetemplate=$(echo ${filepath}/4.LDhat_output/case_${n}/SLiM_case_${n}_)
 
 Rscript --vanilla 6.aux/SLiM_Parameters.txt 7.LDhat_summary/LDhat_case_ .summary.txt LDhat_
