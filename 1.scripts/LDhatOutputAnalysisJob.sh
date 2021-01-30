df1d9a3b1a40a03eb57e7778e80cdb5ccc4098ec
Enrique(mac) <ejschwarzkopf@gmail.com>
2021-01-30 12:00:40 -0800

    debugging

--- 1.scripts/LDhatOutputAnalysisJob.sh
+++ 1.scripts/LDhatOutputAnalysisJob.sh
@@ -18,7 +18,7 @@ filetemplate=$(echo ${filepath}/4.LDhat_output/case_${n}/SLiM_case_${n}_)
 
 Rscript --vanilla 1.scripts/LDhatOutputAnalysis.R ${filetemplate} 500
 
-if [ $n -eq 900]; then
+if [ $n -eq 900 ]; then
 	for i in `seq 1 900`; do
 		cp ${filepath}/4.LDhat_output/case_${i}/SLiM_case_${i}_.res.summary.txt 7.LDhat_summary/LDhat_case_${i}.summary.txt
 	done
