1a99ea71f5d726156f3f16de259e877723592ed1
Enrique(mac) <ejschwarzkopf@gmail.com>
2021-02-01 16:58:53 -0800

    debugging

--- 1.scripts/LDhatFigures.R
+++ 1.scripts/LDhatFigures.R
@@ -68,7 +68,7 @@ diff_full_table<-data.frame(rep(0, 500))
 for( i in param_diff_table ){
 	diff_case_name<-paste(input_template_prefix_case, i, input_template_suffix_case, sep = '')
 	diff_case_table<-read.table(diff_case_name, header=TRUE, row.names=1)
-	diff_case_vector<-diff_case_name$Rates_diff
+	diff_case_vector<-diff_case_table$Rates_diff
 	diff_figure_table<-cbind(diff_figure_table, diff_case_vector)
 	rm(diff_case_table)
 }
