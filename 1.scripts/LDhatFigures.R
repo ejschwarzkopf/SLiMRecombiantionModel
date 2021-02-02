45f2cb3873c5d5bad9107cd95c3900aa28c185c7
Enrique(mac) <ejschwarzkopf@gmail.com>
2021-02-01 16:59:46 -0800

    debugging

--- 1.scripts/LDhatFigures.R
+++ 1.scripts/LDhatFigures.R
@@ -66,6 +66,7 @@ print("mediansd")
 diff_full_table<-data.frame(rep(0, 500))
 
 for( i in param_diff_table ){
+	print(i)
 	diff_case_name<-paste(input_template_prefix_case, i, input_template_suffix_case, sep = '')
 	diff_case_table<-read.table(diff_case_name, header=TRUE, row.names=1)
 	diff_case_vector<-diff_case_table$Rates_diff
